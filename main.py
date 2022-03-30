b5a8c27d3568f7aa941848a45b260a5bbfd4276b
ProfSathishR <30977778+Professor-Sathish@users.noreply.github.com>
2022-03-30 16:41:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import streamlit as st
+import cv2
+import streamlit.components.v1 as components
 from IPython import display
-id=input("Enter Google Scholar ID : ")
-ur="https://scholar.google.co.in/citations?user="+id+"&hl=en"
-display.HTML(url=ur)
+
+
+st.title("Interview Questions")
+name = st.text_input("Enter your Name:")
+email = st.text_input("Enter your Email:")
+affiliation = st.text_input("Affiliation:")
+area_of_intrest = st.text_input("Area of Intrest: ")
+webpage = st.text_input("Home Page (eg : https://www.yourwebpage.com)")
+code = st.text_input("Enter your 12-Digit Code:", max_chars=12)
+camera_btn = st.button("Take photo")
+if camera_btn:
+  pass
+    
+
+if len(code) == 12:
+    ur="https://scholar.google.co.in/citations?user="+code+"&hl=en"
+    st.markdown(display.HTML(url=ur))
+
+
+
